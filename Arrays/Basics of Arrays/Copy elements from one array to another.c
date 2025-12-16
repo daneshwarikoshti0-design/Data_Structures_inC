e4283b0b4afeef546764898897fd516fedb8fe49
Daneshwari Koshti <daneshwarikoshti0@gmail.com>
2025-12-16 14:48:34 +0530

    Create Copy elements from one array to another.c

---
+++ Arrays/Basics of Arrays/Copy elements from one array to another.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    int n, i;
+    int arr1[100], arr2[100];
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr1[i]);
+    }
+
+    // Copy elements
+    for (i = 0; i < n; i++) {
+        arr2[i] = arr1[i];
+    }
+
+    printf("Copied array elements:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr2[i]);
+    }
+
+    return 0;
+}
