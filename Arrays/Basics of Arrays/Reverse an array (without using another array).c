b3fc585de5216075b727113ecdb39d5ca81f70fd
Daneshwari Koshti <daneshwarikoshti0@gmail.com>
2025-12-16 14:45:39 +0530

    Create Reverse an array (without using another array).c

---
+++ Arrays/Basics of Arrays/Reverse an array (without using another array).c
@@ -0,0 +1,29 @@
+//The first element is swapped with the last, The second with the second last, and so on.
+#include <stdio.h>
+int main() {
+    int n, i;
+    int arr[100];
+    int temp;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Reverse the array in place
+    for (i = 0; i < n / 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = temp;
+    }
+
+    printf("Reversed array:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
