8a300fade8d5944c5608896fa1771f93fd7b4861
Daneshwari Koshti <daneshwarikoshti0@gmail.com>
2025-12-23 21:58:22 +0530

    Create Merge two arrays into a third array.c

---
+++ Arrays/Computation & Frequency/Merge two arrays into a third array.c
@@ -0,0 +1,40 @@
+//concatenate it without deleting duplicates items
+#include <stdio.h>
+
+int main() {
+    int n1, n2, i;
+    int a[100], b[100], c[200];
+
+    printf("Enter number of elements in first array: ");
+    scanf("%d", &n1);
+
+    printf("Enter %d elements of first array:\n", n1);
+    for (i = 0; i < n1; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter number of elements in second array: ");
+    scanf("%d", &n2);
+
+    printf("Enter %d elements of second array:\n", n2);
+    for (i = 0; i < n2; i++) {
+        scanf("%d", &b[i]);
+    }
+
+    // Copy first array into third array
+    for (i = 0; i < n1; i++) {
+        c[i] = a[i];
+    }
+
+    // Copy second array into third array
+    for (i = 0; i < n2; i++) {
+        c[n1 + i] = b[i];
+    }
+
+    printf("Merged array:\n");
+    for (i = 0; i < n1 + n2; i++) {
+        printf("%d ", c[i]);
+    }
+
+    return 0;
+}
