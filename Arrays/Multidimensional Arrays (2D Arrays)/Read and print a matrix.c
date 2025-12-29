84da9e8095f8fcc2baef7d645976e84e225010f1
Daneshwari Koshti <daneshwarikoshti0@gmail.com>
2025-12-29 23:09:23 +0530

    Create Read and print a matrix.c

---
+++ Arrays/Multidimensional Arrays (2D Arrays)/Read and print a matrix.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    int i, j;
+    int matrix[10][10];
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    printf("Enter elements of the matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("\nMatrix elements are:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
