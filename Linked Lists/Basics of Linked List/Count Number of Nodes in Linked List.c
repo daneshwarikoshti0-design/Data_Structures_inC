f28ef0803993deca4fd74997ca38a5fd0b5f2eaa
Daneshwari Koshti <daneshwarikoshti0@gmail.com>
2026-02-09 11:21:24 +0530

    Create Count Number of Nodes in Linked List.c

---
+++ Linked Lists/Basics of Linked List/Count Number of Nodes in Linked List.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+int main() {
+    struct node *head = NULL, *temp, *newnode;
+    int n, i, count = 0;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        newnode = (struct node*)malloc(sizeof(struct node));
+        scanf("%d", &newnode->data);
+        newnode->next = NULL;
+
+        if (head == NULL)
+            head = temp = newnode;
+        else {
+            temp->next = newnode;
+            temp = newnode;
+        }
+    }
+
+    temp = head;
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+
+    printf("Number of nodes = %d", count);
+
+    return 0;
+}
