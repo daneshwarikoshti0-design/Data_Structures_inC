c1fa130e9e5005ae475f32afe9561dc14b65a170
Daneshwari Koshti <daneshwarikoshti0@gmail.com>
2026-02-09 20:41:23 +0530

    Create Insert a Node at the Beginning.c

---
+++ Linked Lists/Basics of Linked List/Insert a Node at the Beginning.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+int main() {
+    struct node *head = NULL, *newnode, *temp;
+    int n, i;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        newnode = (struct node*)malloc(sizeof(struct node));
+        printf("Enter data: ");
+        scanf("%d", &newnode->data);
+
+        newnode->next = head;
+        head = newnode;
+    }
+
+    printf("Linked List:\n");
+    temp = head;
+    while(temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL");
+
+    return 0;
+}
