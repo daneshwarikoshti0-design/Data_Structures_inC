bc57acae68abf43c57edc6dfd74af77f4c23d3d5
Daneshwari Koshti <daneshwarikoshti0@gmail.com>
2026-02-10 14:32:37 +0530

    Update Reverse Doubly Linked List.c

--- Linked Lists/Doubly Linked List Programs/Reverse Doubly Linked List.c
+++ Linked Lists/Doubly Linked List Programs/Reverse Doubly Linked List.c
@@ -1,21 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct NODE{
+struct NODE {
     int data;
-    struct NODE *prev,*next;
+    struct NODE *prev, *next;
 };
 
-struct NODE* reverse(struct NODE* head){
-    struct NODE *temp=NULL,*curr=head;
+struct NODE* reverse(struct NODE* head) {
+    struct NODE *temp = NULL, *curr = head;
+    if(!head) return NULL;
 
-    while(curr){
-        temp=curr->prev;
-        curr->prev=curr->next;
-        curr->next=temp;
-        curr=curr->prev;
+    while (curr != NULL) {
+        temp = curr->prev;
+        curr->prev = curr->next;
+        curr->next = temp;
+        curr = curr->prev; // Move to next node
+    }
+    if (temp != NULL) head = temp->prev;
+    return head;
+}
+
+int main() {
+    struct NODE *head = NULL, *newNode, *temp;
+    int n, val;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++) {
+        printf("Enter value: ");
+        scanf("%d", &val);
+        newNode = (struct NODE*)malloc(sizeof(struct NODE));
+        newNode->data = val; newNode->next = NULL;
+        if(!head) { head = newNode; newNode->prev = NULL; }
+        else {
+            temp = head;
+            while(temp->next) temp = temp->next;
+            temp->next = newNode; newNode->prev = temp;
+        }
     }
 
-    if(temp) head=temp->prev;
-    return head;
+    head = reverse(head);
+    printf("Reversed List: ");
+    while(head) { printf("%d <-> ", head->data); head = head->next; }
+    printf("NULL\n");
+    return 0;
 }
