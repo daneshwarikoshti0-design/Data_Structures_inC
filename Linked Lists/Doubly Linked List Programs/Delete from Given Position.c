649f6819f0036b6483f2c68981a5a52159cf0e97
Daneshwari Koshti <daneshwarikoshti0@gmail.com>
2026-02-10 14:42:30 +0530

    Update Delete from Given Position.c

--- Linked Lists/Doubly Linked List Programs/Delete from Given Position.c
+++ Linked Lists/Doubly Linked List Programs/Delete from Given Position.c
@@ -1,52 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct NODE{
+struct NODE {
     int data;
-    struct NODE *prev,*next;
+    struct NODE *prev, *next;
 };
 
-struct NODE* createNode(int val){
-    struct NODE* n=(struct NODE*)malloc(sizeof(struct NODE));
-    n->data=val;
-    n->prev=n->next=NULL;
-    return n;
+// ... (createNode and insertEnd functions same as above) ...
+struct NODE* createNode(int val) {
+    struct NODE* newNode = (struct NODE*)malloc(sizeof(struct NODE));
+    newNode->data = val; newNode->prev = newNode->next = NULL;
+    return newNode;
 }
 
-struct NODE* insertPosition(struct NODE* head,int val,int pos){
-    struct NODE* newNode=createNode(val);
-
-    if(pos==1){
-        newNode->next=head;
-        if(head) head->prev=newNode;
-        return newNode;
-    }
-
-    struct NODE* temp=head;
-    for(int i=1;i<pos-1 && temp;i++)
-        temp=temp->next;
-
-    if(temp==NULL) return head;
-
-    newNode->next=temp->next;
-    newNode->prev=temp;
-    if(temp->next) temp->next->prev=newNode;
-    temp->next=newNode;
-
+struct NODE* insertEnd(struct NODE* head, int val) {
+    struct NODE* newNode = createNode(val);
+    if (!head) return newNode;
+    struct NODE* temp = head;
+    while (temp->next) temp = temp->next;
+    temp->next = newNode; newNode->prev = temp;
     return head;
 }
 
-void display(struct NODE* head){
-    while(head){
-        printf("%d <-> ",head->data);
-        head=head->next;
+struct NODE* deleteAtPos(struct NODE* head, int pos) {
+    if (!head) return NULL;
+    struct NODE* temp = head;
+
+    if (pos == 1) {
+        head = head->next;
+        if (head) head->prev = NULL;
+        free(temp);
+        return head;
     }
+
+    for (int i = 1; temp != NULL && i < pos; i++) temp = temp->next;
+
+    if (!temp) { printf("Position invalid!\n"); return head; }
+
+    if (temp->next) temp->next->prev = temp->prev;
+    if (temp->prev) temp->prev->next = temp->next;
+
+    free(temp);
+    printf("Node at position %d deleted.\n", pos);
+    return head;
 }
 
-int main(){
-    struct NODE* head=NULL;
-    head=insertPosition(head,10,1);
-    head=insertPosition(head,20,2);
-    head=insertPosition(head,15,2);
-    display(head);
+int main() {
+    struct NODE* head = NULL;
+    int n, val, pos;
+
+    printf("How many nodes? ");
+    scanf("%d", &n);
+    for(int i=0; i<n; i++) {
+        printf("Value: "); scanf("%d", &val);
+        head = insertEnd(head, val);
+    }
+
+    printf("Enter position to delete: ");
+    scanf("%d", &pos);
+    head = deleteAtPos(head, pos);
+
+    printf("Final List: ");
+    while(head) { printf("%d ", head->data); head = head->next; }
+    return 0;
 }
