a2024ad1feae7b301af97c8182580dc6e7df79ad
Daneshwari Koshti <daneshwarikoshti0@gmail.com>
2026-02-10 14:22:37 +0530

    Update Sort Doubly Linked List.c

--- Linked Lists/Doubly Linked List Programs/Sort Doubly Linked List.c
+++ Linked Lists/Doubly Linked List Programs/Sort Doubly Linked List.c
@@ -1,22 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct NODE{
+struct NODE {
     int data;
-    struct NODE *prev,*next;
+    struct NODE *prev, *next;
 };
 
-void sort(struct NODE* head){
-    struct NODE *i,*j;
-    int temp;
+void sortList(struct NODE* head) {
+    int swapped, tempVal;
+    struct NODE *ptr1;
+    struct NODE *lptr = NULL;
 
-    for(i=head;i;i=i->next){
-        for(j=i->next;j;j=j->next){
-            if(i->data>j->data){
-                temp=i->data;
-                i->data=j->data;
-                j->data=temp;
+    if (head == NULL) return;
+
+    do {
+        swapped = 0;
+        ptr1 = head;
+
+        while (ptr1->next != lptr) {
+            if (ptr1->data > ptr1->next->data) { 
+                // Swap data
+                tempVal = ptr1->data;
+                ptr1->data = ptr1->next->data;
+                ptr1->next->data = tempVal;
+                swapped = 1;
             }
+            ptr1 = ptr1->next;
         }
-    }
+        lptr = ptr1;
+    } while (swapped);
+}
+
+int main() {
+    struct NODE *n1 = (struct NODE*)malloc(sizeof(struct NODE));
+    struct NODE *n2 = (struct NODE*)malloc(sizeof(struct NODE));
+    struct NODE *n3 = (struct NODE*)malloc(sizeof(struct NODE));
+    n1->data = 50; n2->data = 10; n3->data = 30;
+    n1->next = n2; n2->prev = n1; n2->next = n3; n3->prev = n2; n3->next = NULL;
+
+    printf("Before Sort: 50, 10, 30\n");
+    sortList(n1);
+    
+    printf("After Sort: ");
+    struct NODE* t = n1;
+    while(t) { printf("%d ", t->data); t = t->next; }
+    return 0;
 }
