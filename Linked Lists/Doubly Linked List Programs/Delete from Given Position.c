6e557f3a96e026f2c900e8516cc927aea8601934
Daneshwari Koshti <daneshwarikoshti0@gmail.com>
2026-02-10 11:52:35 +0530

    Update Delete from Given Position.c

--- Linked Lists/Doubly Linked List Programs/Delete from Given Position.c
+++ Linked Lists/Doubly Linked List Programs/Delete from Given Position.c
@@ -1,31 +1,52 @@
-struct NODE* deletePosition(struct NODE *head,int pos){
-    if(head==NULL)
-        return NULL;
+#include <stdio.h>
+#include <stdlib.h>
 
-    struct NODE *temp=head;
+struct NODE{
+    int data;
+    struct NODE *prev,*next;
+};
+
+struct NODE* createNode(int val){
+    struct NODE* n=(struct NODE*)malloc(sizeof(struct NODE));
+    n->data=val;
+    n->prev=n->next=NULL;
+    return n;
+}
+
+struct NODE* insertPosition(struct NODE* head,int val,int pos){
+    struct NODE* newNode=createNode(val);
 
     if(pos==1){
-        head=head->next;
-        if(head!=NULL)
-            head->prev=NULL;
-        free(temp);
-        return head;
+        newNode->next=head;
+        if(head) head->prev=newNode;
+        return newNode;
     }
 
-    for(int i=1;i<pos && temp!=NULL;i++)
+    struct NODE* temp=head;
+    for(int i=1;i<pos-1 && temp;i++)
         temp=temp->next;
 
-    if(temp==NULL){
-        printf("Invalid Position\n");
-        return head;
-    }
+    if(temp==NULL) return head;
 
-    if(temp->next!=NULL)
-        temp->next->prev=temp->prev;
+    newNode->next=temp->next;
+    newNode->prev=temp;
+    if(temp->next) temp->next->prev=newNode;
+    temp->next=newNode;
 
-    if(temp->prev!=NULL)
-        temp->prev->next=temp->next;
-
-    free(temp);
     return head;
+}
+
+void display(struct NODE* head){
+    while(head){
+        printf("%d <-> ",head->data);
+        head=head->next;
+    }
+}
+
+int main(){
+    struct NODE* head=NULL;
+    head=insertPosition(head,10,1);
+    head=insertPosition(head,20,2);
+    head=insertPosition(head,15,2);
+    display(head);
 }
